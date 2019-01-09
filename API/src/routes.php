07d1772d65dac260c2d595cf18b614f090a4ee8a
RainyRuRu <believeu123@gmail.com>
2019-01-09 22:34:54 +0800

    Add daily-chart & hourly-chart

--- API/src/routes.php
+++ API/src/routes.php
@@ -2,9 +2,13 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use App\Controllers\MainController;
 
 // Routes
 
 $app->get('/', function (Request $request, Response $response, array $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/daily-chart', MainController::class . ':fetchDailyChart');
+$app->get('/hourly-chart', MainController::class . ':fetchHourlyChart');
