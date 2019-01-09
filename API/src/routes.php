be3ca5f440f3e36f4713b7f92f82c10d1440e4a1
RainyRuRu <believeu123@gmail.com>
2019-01-09 11:20:50 +0800

    Init API

---
+++ API/src/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// Routes
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
